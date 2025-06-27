0b6eb39ad16b405e9072adc33d0135d60be9467a
dustoonv <dustinvans7@gmail.com>
2025-06-26 22:12:20 -0500

    first commit

---
+++ scenes/player.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+@export var speed = 200
+@onready var sprite = $AnimatedSprite2D
+
+func get_input():
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = direction * speed
+	print(direction)
+	if direction != Vector2.ZERO:
+		if abs(direction.x) > abs(direction.y):
+			sprite.animation = "right" if direction.x > 0 else "left"
+		else:
+			sprite.animation = "down" if direction.y > 0 else "up"
+		sprite.play()
+	else:
+		sprite.stop()
+		
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
