dc7dde6e931456780bd6a21c83cfedd6db0025b4
dustoonv <dustinvans7@gmail.com>
2025-06-28 13:16:29 -0500

    damage system

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,12 +1,19 @@
 extends CharacterBody2D
 
 const SPEED = 100
+const INVINCIBILITY_TIME = 1
+const KNOCKBACK_MAGNITUDE = 200
+const KNOCKBACK_TIME = .1
+
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @export var health = 100
+@export var is_invincible = false
+var knockback: Vector2 = Vector2.ZERO
+var is_knockback: bool = false
 
 func _get_input() -> void:
 	var direction: Vector2 = Input.get_vector("left", "right", "up", "down")
-	velocity = direction * SPEED
+	velocity = direction * SPEED 
 	
 	if direction != Vector2.ZERO:
 		if abs(direction.x) >= abs(direction.y):
@@ -18,19 +25,49 @@ func _get_input() -> void:
 		sprite.stop()
 		
 func _physics_process(delta) -> void:
-	_get_input()
+	if not is_knockback:
+		_get_input()
 	move_and_slide()
 	
-func _blink_red(blinks: int = 3, interval: float = 0.1) -> void:
-	for i in blinks:
-		modulate = Color(1, 0, 0)  # Red
+func _blink_damage(interval: float = 0.1) -> void:
+	modulate = Color(1, 0, 0) 
+	await get_tree().create_timer(interval).timeout
+
+	modulate = Color(1, 1, 1) 
+	await get_tree().create_timer(interval).timeout
+
+func _blink_invincibility(interval: float = 0.1):
+	while is_invincible:
+		modulate.a = 0.5  
 		await get_tree().create_timer(interval).timeout
 
-		modulate = Color(1, 1, 1)  # Normal
+		modulate.a = 1 
 		await get_tree().create_timer(interval).timeout
-	
-func take_damage(amount) -> void:
-	health -= amount
-	
-	_blink_red()
+
+func _give_invincibility() -> void:
+	is_invincible = true
+	_blink_invincibility()
+	await get_tree().create_timer(INVINCIBILITY_TIME).timeout
+	is_invincible = false
+
+func _apply_knockback(knockback: Vector2):
+		is_knockback = true
+		velocity = knockback.normalized() * KNOCKBACK_MAGNITUDE
+
+		await get_tree().create_timer(KNOCKBACK_TIME).timeout
+		is_knockback = false
+		velocity = Vector2.ZERO
+		
+		#TODO: play knockback animation
+		sprite.stop()
+		
+func take_damage(amount: int, knockback: Vector2) -> void:
+	if not is_invincible:
+		_blink_damage()
+		_give_invincibility()
+		
+		_apply_knockback(knockback)
+		health -= amount
+		
+
 	
--- scenes/slime.gd
+++ scenes/slime.gd
@@ -2,10 +2,12 @@ extends CharacterBody2D
 
 const SPEED = 10 
 const DAMAGE = 10
+const DAMAGE_INTERVAL = 0.5
 const PLAYER_PATH: NodePath = "../Player"
 
 # Forward declaration. Character needs to exist before finding position
-var player : CharacterBody2D 
+var player: CharacterBody2D 
+var slimeInPlayer: bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,10 +19,15 @@ func _ready() -> void:
 	
 	var area2d : Area2D = $Area2D
 	area2d.body_entered.connect(_body_entered)
+	area2d.body_exited.connect(_body_exited)
 	
-
+func _body_exited(body: Node2D) -> void:
+	slimeInPlayer = false
 func _body_entered(body: Node2D) -> void:
-	body.take_damage(DAMAGE)
+	slimeInPlayer = true
+	while slimeInPlayer:
+		body.take_damage(10, velocity)
+		await get_tree().create_timer(DAMAGE_INTERVAL).timeout
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
