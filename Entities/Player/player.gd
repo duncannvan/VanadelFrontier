57a860d10d32ca00267bce6e653f157bc08ead5e
dustoonv <dustinvans7@gmail.com>
2025-07-23 19:28:49 -0500

    feat added animation tree for states

--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -1,67 +1,107 @@
 class_name Player extends CombatUnit
 
 enum State {
-	IDLE = 0x1 << 0, 
-	WALKING = 0x1 << 1, 
-	ATTACKING = 0x1 << 2, 
-	KNOCKEDBACK = 0x1 << 3, 
-	DEAD = 0x1 << 4,
+	IDLE, 
+	RUNNING, 
+	ATTACKING, 
+	KNOCKEDBACK, 
+	DEAD,
 }
 
-@export var _invincibility_time: float = 1.0
+const COMBO_LIMIT: int = 4
+const COMBO_WINDOW_TIME: float = 0.5
 
 var _state: State = State.IDLE
 var _last_facing_direction: Vector2 = Vector2.DOWN
+var attacking: bool = false
+var combo_step: int = 0 :
+	set(value):
+		combo_step = wrapi(value, 1, COMBO_LIMIT)
 
 @onready var _hitbox: HitBox = $HitBox
 @onready var _hurtbox: HurtBox = $HurtBox
-@onready var _damaged_effects_animation: AnimationPlayer = $DamagedEffectsAnimation
-@onready var _invincibility_effects_animation: AnimationPlayer = $InvincibilityEffectsAnimation
-@onready var _sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var _stats_component: StatsComponents = $StatsComponents
 @onready var _player_camera: Camera2D = $PlayerCamera
 @onready var _animation_tree: AnimationTree = $AnimationTree
+@onready var _animation_player: AnimationPlayer = $AnimationPlayer
+@onready var _combat_effects: AnimationPlayer = $CombatEffects
+@onready var _combo_timer: Timer = $ComboTimer
 
-
-func _init() -> void:
-	set_collision_layer(2)
-	
 
 func _ready() -> void:
 	_hurtbox.hurtbox_entered.connect(_apply_attack_effects)
 	_stats_component.died.connect(_die)
+	_update_blend_positions(_last_facing_direction)
 	
-	
+	_combo_timer.timeout.connect(_on_combo_timer_timeout)
+
+
 func _physics_process(delta: float) -> void:
-	if _is_state(State.DEAD) or _is_state(State.KNOCKEDBACK): 
+	match _state:
+		State.IDLE:
+			_handle_idle()
+		State.RUNNING:
+			_handle_running()
+		State.ATTACKING:
+			pass
+		State.KNOCKEDBACK:
+			move_and_slide()
+		State.DEAD:
+			pass	
+
+
+func _set_state(new_state: State) -> void:
+	if _state == new_state:
 		return
 	
+	_state = new_state
+	
+	# Do something after transition to new state
+	match _state:
+		State.ATTACKING:
+			combo_step += 1
+			print(combo_step)
+			_combo_timer.start(_animation_player.get_animation("slash_down_%d" % combo_step).length + COMBO_WINDOW_TIME)
+
+
+func end_attack():
+	_set_state(State.IDLE)
+
+
+func _on_combo_timer_timeout() -> void:
+	combo_step = 0
+
+#func _handle_attacking() -> void:
+	#if Input.is_action_just_pressed("attack"):
+		#combo_step += 1
+		#print(combo_step)
+
+
+func _handle_idle() -> void:
+	if Input.get_vector("left", "right", "up", "down"):
+		_set_state(State.RUNNING)
+	elif Input.is_action_just_pressed("attack"):
+		_set_state(State.ATTACKING)
+
+
+func _handle_running():
 	var direction = Input.get_vector("left", "right", "up", "down")
 	
-	if direction: 
+	if direction == Vector2.ZERO:
+		_set_state(State.IDLE)
+	elif Input.is_action_just_pressed("attack"):
+		_set_state(State.ATTACKING)
+	else:
 		_last_facing_direction = direction
-		_update_blend_positions(direction)
-	
-	if Input.is_action_pressed("attack") and not _is_state(State.ATTACKING):
-		_attack_handler()
+		_update_blend_positions(_last_facing_direction)
 		
-	move_and_slide()
-
-
+		velocity = direction * _stats_component.get_current_speed()
+		move_and_slide()
+	
 
 func apply_damage(damage: int, hitbox_position: Vector2) -> void:
-	_damaged_effects_animation.play("damaged_effects")
+	_combat_effects.play("damaged_effects")
 	_stats_component.apply_damage(damage)
-	_give_invincibility()
-
-
-func _give_invincibility() -> void:
-	_hurtbox.set_invincible(true)
-	_invincibility_effects_animation.play("invincibility_effects")
-	await get_tree().create_timer(_invincibility_time).timeout
-	_hurtbox.set_invincible(false)
-	_invincibility_effects_animation.seek(0.0) 
-	_invincibility_effects_animation.stop()
 
 
 func apply_slow(slowed_factor: float, slowed_duration: int) -> void:
@@ -69,77 +109,43 @@ func apply_slow(slowed_factor: float, slowed_duration: int) -> void:
 
 
 func apply_knockback(knockback_vector := Vector2.ZERO, knockback_duration: float = 0.0) -> void:
-	_sprite.stop()
-	_add_state(State.KNOCKEDBACK)
+	_set_state(State.KNOCKEDBACK)
 	velocity = knockback_vector
 	await get_tree().create_timer(knockback_duration).timeout
 	velocity = Vector2.ZERO
-	_exit_state(State.KNOCKEDBACK)
+	_set_state(State.IDLE)
 
 
 func _die() -> void:
-	_add_state(State.DEAD)
+	_set_state(State.DEAD)
 	queue_free()
 	Global.game_over.emit()
 
 
-#func _walk_handler(direction: Vector2) -> void:
-	#velocity = direction * _stats_component.get_current_speed() 
-	#
-	#if direction != Vector2.ZERO:
-		#if abs(direction.x) >= abs(direction.y):
-			#_sprite.animation = "right" if direction.x > 0 else "left"
-		#else:
-			#_sprite.animation = "down" if direction.y > 0 else "up"
-		#_sprite.play()
-		#_add_state(State.WALKING)
-		#_exit_state(State.IDLE)
-	#else:
-		#_sprite.stop()
-		#_add_state(State.IDLE)
-		#_exit_state(State.WALKING)
-
-
-# TODO: Move into weapon/attack node
-func _attack_handler() -> void:
-	#match _last_facing_direction:
-		#Vector2.DOWN:
-			#_tool_animations.play("slash_down")
-		#Vector2.DOWN:
-			#_tool_animations.play("slash_down")
-		#Vector2.DOWN:
-			#_tool_animations.play("slash_down")
-		#Vector2.DOWN:
-			#_tool_animations.play("slash_down")
-	#
-	_add_state(State.ATTACKING)
-	
-	#_hitbox.set_hitbox_enable(true)
-	#_hitbox.get_node("HitEffects").visible = true 
-	#await get_tree().create_timer(.1).timeout
-	#_hitbox.get_node("HitEffects").visible = false
-	#_hitbox.set_hitbox_enable(false)
-	await get_tree().create_timer(.3).timeout # Scuffed attack cooldown
-	_exit_state(State.ATTACKING)
-
-
 func _update_blend_positions(direction: Vector2) -> void:
-	_animation_tree.set("p	arameters/StateMachine/MoveState/RunState/blend_position", direction)
+	_animation_tree.set("parameters/StateMachine/MoveState/RunState/blend_position", direction)
 	_animation_tree.set("parameters/StateMachine/MoveState/IdleState/blend_position", direction)
+	_animation_tree.set("parameters/StateMachine/AttackState/Slash1/blend_position", direction)
+	_animation_tree.set("parameters/StateMachine/AttackState/Slash2/blend_position", direction)
+	_animation_tree.set("parameters/StateMachine/AttackState/Slash3/blend_position", direction)
 
 
 func _apply_attack_effects(hitbox: HitBox) -> void:
 	for effect in hitbox.attack_effects:
 			effect.apply(self, hitbox.global_position)
-	
-
-func _add_state(state: State) -> void:
-	_state |= state
 
 
-func _is_state(state: State) -> bool:
-	return _state & state 
+#func _add_state(state: State) -> void:
+	#_state |= state
+#
+#
+#func _is_state(state: State) -> bool:
+	#return _state & state 
+#
+#
+#func _exit_state(state: State) -> void :
+	#_state &= ~state
 
 
-func _exit_state(state: State) -> void :
-	_state &= ~state
+#func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	#print(anim_name)
