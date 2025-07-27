ec558ec4d82a088ac8dc12ea59bea38d11ed3be4
Duncan Van <86893568+duncannvan@users.noreply.github.com>
2025-07-27 12:02:29 -0500

    Cooldown (#15)
    * tool bar ui draft
    
    * tool bar prototype done
    
    * hover png
    
    * cleanup
    
    * tool bar ui refactor
    
    * merege main
    
    * new slot png
    
    * ani tree bug
    
    * initial cooldown draft
    
    * error checks
    
    * cleanup
    
    * print cleanup
    
    ---------
    
    Co-authored-by: dustoonv <dustinvans7@gmail.com>

--- Components/tool_manager.gd
+++ Components/tool_manager.gd
@@ -2,20 +2,30 @@ class_name ToolManager extends Node
 
 signal selected_slot_changed(slot_idx: int)
 signal toolbar_modified(tools: Array[ToolResource])
+signal tool_used()
 
 const NUM_TOOL_SLOTS: int = 5
 const NO_TOOL_SELECTED: int = -1
+const COMBO_WINDOW_SEC: float = 0.5
 
 @export var _tool_resources: Array[ToolResource] = []
 
 var _selected_tool_idx: int = NO_TOOL_SELECTED
-var _blend_point_idx_map: Dictionary[String, int] = {"left": 0, "right": 0, "down": 0, "up": 0} 
+var _blend_point_idx_map: Dictionary[String, int] = {"left": 0, "right": 0, "down": 0, "up": 0}
+var _combo_timer: Timer = null
+
+func _ready() -> void:
+	_combo_timer = Timer.new()
+	add_child(_combo_timer)
+	_combo_timer.timeout.connect(_on_combo_timer_expire)
 
 
 # Public Methods:
 # Param slot_postion: 0 indexed slot position for selecting a tool
 func set_selected_tool(slot_position: int, anim_tree: AnimationTree) -> void:
 	if slot_position < NUM_TOOL_SLOTS:
+		if is_tool_selected():
+			_get_selected_tool().reset_lib_idx()
 		emit_signal("selected_slot_changed", slot_position)
 	
 	if slot_position >= _tool_resources.size() or slot_position == _selected_tool_idx or !_tool_resources[slot_position]:
@@ -26,11 +36,27 @@ func set_selected_tool(slot_position: int, anim_tree: AnimationTree) -> void:
 	set_tool_animation(anim_tree)
 
 
-func get_selected_tool() -> ToolResource:
-	assert(_selected_tool_idx != NO_TOOL_SELECTED,"No tool is selected. Use is_tool_selected() or do a null check in the client")
-	return _tool_resources[_selected_tool_idx]
+func use_selected_tool(_animation_tree: AnimationTree) -> void:
+	if not is_tool_selected() and _get_selected_tool().cooling_down:
+		return
+	
+	# Save idx here to restore this tool's cool_down bool incase the selected tool idx changes
+	var tool_idx: int = _selected_tool_idx
+	var cooldown_sec: float = _get_selected_tool().get_cooldown_sec()
+	
+	_get_selected_tool().cooling_down = true
+	
+	# Only update animation if there multiple animation libs in the tool
+	if _get_selected_tool().animation_libs.size() > 1:
+		set_tool_animation(_animation_tree, _get_selected_tool().get_lib_idx())
+		_combo_timer.start(cooldown_sec + COMBO_WINDOW_SEC)
 
-
+	emit_signal("tool_used")
+	_get_selected_tool().use_tool()
+	await get_tree().create_timer(cooldown_sec).timeout
+	_tool_resources[tool_idx].cooling_down = false
+ 
+	
 func is_tool_selected() -> bool:
 	return _selected_tool_idx != NO_TOOL_SELECTED
 	
@@ -43,6 +69,7 @@ func remove_tool(slot_position: int) -> void:
 	if _tool_resources[slot_position]:
 		_tool_resources[slot_position] = null
 	emit_signal("toolbar_modified", get_all_tools())
+
 
 func add_new_tool(tool: ToolResource) -> bool:
 	if _tool_resources.size() >= NUM_TOOL_SLOTS:
@@ -60,6 +87,7 @@ func swap_tool_slots(tool_idx1: int, tool_idx2: int) -> void:
 		_tool_resources[tool_idx2] = temp
 		emit_signal("toolbar_modified", get_all_tools())
 
+
 # The blend space points are mapped to a decimal index based on the order of creation in the animation tree
 # Get the blend point position vector to determine the corresponding direction for the blend point
 # This must be called once in the client's _ready() if they intend to use tool animations
@@ -70,7 +98,7 @@ func set_blend_point_idx_mapping(anim_tree: AnimationTree) -> void:
 		var direction: String = _vector_to_direction(blend_point_pos)
 		_blend_point_idx_map[direction] = blend_space_idx
 	
-	
+
 # Swaps the tool animation
 # Param lib_idx: Tools can have multiple animation libraries. The idx is used to determined which library to use. 
 #                Defaulted to 0 since most tools will only have 1 library.
@@ -78,18 +106,18 @@ func set_tool_animation(anim_tree: AnimationTree, lib_idx: int = 0) -> void:
 	var blend_space: AnimationNodeBlendSpace2D = anim_tree.tree_root.get_node("StateMachine").get_node("ToolState")
 	var tool_lib_name: String = ""
 	
-	assert(get_selected_tool(), "Tool doesn't not equipped")
-	assert(lib_idx < get_selected_tool().animation_libs.size(), "Must add animation library to the tool resource to use with the given idx")
+	assert(_get_selected_tool(), "Tool doesn't not equipped")
+	assert(lib_idx < _get_selected_tool().animation_libs.size(), "Must add animation library to the tool resource to use with the given idx")
 	
 	# Get local library name of the tool to reference it in the animation player
 	for lib_name: String in anim_tree.get_animation_library_list():
 		var lib = anim_tree.get_animation_library(lib_name)
-		if lib == get_selected_tool().animation_libs[lib_idx]:
+		if lib == _get_selected_tool().animation_libs[lib_idx]:
 			tool_lib_name = lib_name + "/"
 			break
 	
 	# Update the blend point animations for each direction
-	for anim_name: String in get_selected_tool().animation_libs[lib_idx].get_animation_list():
+	for anim_name: String in _get_selected_tool().animation_libs[lib_idx].get_animation_list():
 		var anim_node := AnimationNodeAnimation.new()
 		anim_node.animation = tool_lib_name + anim_name
 		# The local tool animations must include the direction strings
@@ -99,8 +127,18 @@ func set_tool_animation(anim_tree: AnimationTree, lib_idx: int = 0) -> void:
 		
 		
 # Private Methods:
+func _get_selected_tool() -> ToolResource:
+	assert(_selected_tool_idx != NO_TOOL_SELECTED,"No tool is selected. Use is_tool_selected() or do a null check in the client")
+	return _tool_resources[_selected_tool_idx]
+
+
 func _vector_to_direction(vec: Vector2) -> String:
 	if abs(vec.x) > abs(vec.y):
 		return "right" if vec.x > 0 else "left"
 	else:
 		return "down" if vec.y > 0 else "up"
+
+
+func _on_combo_timer_expire() -> void:
+	if is_tool_selected():
+		_get_selected_tool().reset_lib_idx()
--- Resources/ToolResources/melee_weapon.gd
+++ Resources/ToolResources/melee_weapon.gd
@@ -1,7 +1,26 @@
 class_name MeleeWeaponResource extends WeaponResource
 
+const COMBO_LIMIT: int = 3
+var combo_step: int = 0:
+	set(value):
+		combo_step = wrapi(value, 0, COMBO_LIMIT)
+		
 #Override
 func use_tool() -> void:
-	#TODO:Implement
+	combo_step += 1
 	pass
 	
+func get_cooldown_sec() -> float:
+	var first_idx_key = animation_libs[combo_step].get_animation_list()[0]
+	if combo_step >= animation_libs.size() - 1:
+		return animation_libs[combo_step].get_animation(first_idx_key).length + cooldown_sec
+		
+	return animation_libs[combo_step].get_animation(first_idx_key).length
+
+
+func get_lib_idx() -> int:
+	return combo_step
+
+
+func reset_lib_idx() -> void:
+	combo_step = 0
