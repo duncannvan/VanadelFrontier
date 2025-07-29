106ef0fc6110d6850464abdd3e027cbd0da2635e
dustoonv <dustinvans7@gmail.com>
2025-07-29 13:14:55 -0500

    e to open inventory

--- World/inventory_ui.gd
+++ World/inventory_ui.gd
@@ -1,6 +1,10 @@
 extends Control
 
 
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("Inventory"):
+		visible = !visible
+
 func refresh_inventory(inventory: Dictionary[Item, int]) -> void:
 	#TODO: Refresh inventory, if new item add item texture else increment count for existing item
 	pass
