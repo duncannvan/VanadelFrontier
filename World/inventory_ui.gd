860bd003053fb051e08a880c1f3030113a634fac
dustoonv <dustinvans7@gmail.com>
2025-07-29 18:57:04 -0500

    lower case input map

--- World/inventory_ui.gd
+++ World/inventory_ui.gd
@@ -5,7 +5,7 @@ extends Control
 
 
 func _input(event: InputEvent) -> void:
-	if Input.is_action_just_pressed("Inventory"):
+	if Input.is_action_just_pressed("inventory"):
 		visible = !visible
 
 
@@ -13,9 +13,3 @@ func refresh_inventory(inventory: Array[ItemStack]) -> void:
 	for slot_idx: int in len(inventory):
 		slots[slot_idx].get_node("ItemTextureRect").texture = inventory[slot_idx].item.texture
 		slots[slot_idx].get_node("Count").text = str(inventory[slot_idx].count)
-#
-	#var i: int = 0
-	#for stack in inventory:
-		#slots[i].get_node("ItemTextureRect").texture = stack.item.texture
-		#slots[i].get_node("Count").text = str(stack.count)
-		#i += 1
