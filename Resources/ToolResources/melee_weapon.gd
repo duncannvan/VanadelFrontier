082bcafe814901a37f2b34385ebedc1a4b3fc6a0
Duncan Van <vanduncann@gmail.com>
2025-07-28 20:57:40 -0500

    add new api tool resource

--- Resources/ToolResources/melee_weapon.gd
+++ Resources/ToolResources/melee_weapon.gd
@@ -1,26 +1,27 @@
 class_name MeleeWeaponResource extends WeaponResource
 
 const COMBO_LIMIT: int = 3
-var combo_step: int = 0:
+var _pivot_node_path: NodePath = ""
+var _combo_step: int = 0:
 	set(value):
-		combo_step = wrapi(value, 0, COMBO_LIMIT)
+		_combo_step = wrapi(value, 0, COMBO_LIMIT)
 		
 #Override
-func use_tool() -> void:
-	combo_step += 1
+func use_tool(player: Player) -> void:
+	_combo_step += 1
 	pass
 	
 func get_cooldown_sec() -> float:
-	var first_idx_key = animation_libs[combo_step].get_animation_list()[0]
-	if combo_step >= animation_libs.size() - 1:
-		return animation_libs[combo_step].get_animation(first_idx_key).length + cooldown_sec
+	var first_idx_key = animation_libs[_combo_step].get_animation_list()[0]
+	if _combo_step >= animation_libs.size() - 1:
+		return animation_libs[_combo_step].get_animation(first_idx_key).length + cooldown_sec
 		
-	return animation_libs[combo_step].get_animation(first_idx_key).length
+	return animation_libs[_combo_step].get_animation(first_idx_key).length
 
 
 func get_lib_idx() -> int:
-	return combo_step
+	return _combo_step
 
 
 func reset_lib_idx() -> void:
-	combo_step = 0
+	_combo_step = 0
--- Resources/ToolResources/tool.gd
+++ Resources/ToolResources/tool.gd
@@ -8,12 +8,21 @@ class_name ToolResource extends Resource
 @export var cooldown_sec: float = 0 # Time between uses
 @export var animation_libs: Array[AnimationLibrary] = []
 
+
 var cooling_down: bool = false
 
 #Virtual
-func use_tool() -> void:
+func use_tool(player: Player) -> void:
 	push_error("virtual method, use_tool(), must be implemented")
 	
+
+func on_switch_in(player: Player) -> void:
+	pass
+	
+
+func on_switch_out(player: Player) -> void:
+	pass
+
 
 func get_cooldown_sec() -> float:
 	return cooldown_sec
