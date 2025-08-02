bd736f989e88910f188e414b5e78cbc134044f9d
Duncan Van <vanduncann@gmail.com>
2025-08-01 19:05:36 -0500

    cleanup

--- World/crafting_menu_ui.gd
+++ World/crafting_menu_ui.gd
@@ -21,9 +21,10 @@ func _ready() -> void:
 		slot.pressed.connect(_on_craftable_pressed.bind(slot.get_index()))
 		
 	
-func _process(_delta: float):
+func _process(_delta: float) -> void:
 	if placing_tower:
 		tower_inst.global_position = get_viewport().get_camera_2d().get_global_mouse_position()
+		
 		
 # TODO: Move to menu mangager
 func _input(event: InputEvent) -> void:
@@ -31,30 +32,29 @@ func _input(event: InputEvent) -> void:
 		placing_tower = false
 		tower_inst.get_node("TowerBody").disabled = false
 
-	elif  event.is_action_pressed("interact") and all_menus_closed and is_player_in_range:
+	elif event.is_action_pressed("interact") and all_menus_closed and is_player_in_range:
 		visible = !visible
 
 
-func _on_craftable_pressed(slot_idx: int):
+func _on_craftable_pressed(slot_idx: int) -> void:
 	for slot: TextureButton in _slots: 
 		if slot.get_index() != slot_idx:
 			slot.set_pressed(false)
 
 
-func handle_craft_button_press(can_craft: bool):
+func handle_craft_button_press(can_craft: bool) -> void:
 	if not can_craft:
 		return
 	
-	for slot in _slots:
+	for slot: TextureButton in _slots:
 		if slot.is_pressed():
 			tower_inst = tower_scene.instantiate()
-			#tower_inst.global_position = get_viewport().get_camera_2d().get_global_mouse_position()
 			tower_inst.get_node("TowerBody").disabled = true
-			get_parent().get_parent().add_child(tower_inst)
+			get_parent().get_parent().add_child(tower_inst) # TODO: Find better way to add scene to main
 			await get_tree().create_timer(CRAFT_BUTTON_DELAY).timeout
 			placing_tower = true
 			visible = false
 
 
-func update_craft_recipe_ui(num_resources: int):
+func update_craft_recipe_ui(num_resources: int) -> void:
 	_craft_recipe_ui.text = str(num_resources) + "/" + str(tower_resource.craft_recipe.item_stack_count)
